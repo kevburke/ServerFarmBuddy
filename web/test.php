55e031257d919eccbb5b28f27255d469d720af92
Kevin Burke <kevin.burke369@gmail.com>
2016-04-16 22:16:35 +0000

    Php file left out previous commit

---
+++ web/test.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+
+echo "In php";
+$servername = "localhost:3306";
+$username = "root";
+$password = "burke3";
+$dbname = "farmdata";
+echo "Created vars";
+// Create connection
+echo "Con To dbbefore";
+
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+}
+
+
+
+$conn = new mysqli("localhost:3306", "root", "burke3",$dbname);
+echo "Con To db";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM calfie1313426";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br>id: " . $row["jumbo"]. " - Name: " . $row["numID"]. " " . $row["BullName"]. " ". $row["Code"]. " " . $row["MateDate"]. " " . $row["Dob"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+</body>
+</html>
